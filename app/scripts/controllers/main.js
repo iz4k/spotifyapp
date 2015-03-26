239d78f25f5d0dbec3639f138f88203985aa6a0c
iz4k <ilari.meuronen@gmail.com>
2015-03-26 21:35:47 +0200

    first functionality to search and display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,28 @@
  * Controller of the spotifyAppApp
  */
 angular.module('spotifyAppApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, $log) {
+  	$scope.type = 'album';
+    $scope.search = function(){
+    	if($scope.query.length > 3){
+	    	$http.get('https://api.spotify.com/v1/search', {
+	    		params : {
+	    			'q' : $scope.query,
+	    			'type' : 'Album'
+	    		}
+	    	}).success(function(data){
+	    		$scope.albums = data.albums;
+	    		$log.debug($scope.albums);
+	    	});
+	    	$http.get('https://api.spotify.com/v1/search', {
+	    		params : {
+	    			'q' : $scope.query,
+	    			'type' : 'Artist'
+	    		}
+	    	}).success(function(data){
+	    		$scope.artists = data.artists;
+	    		$log.debug($scope.artists);
+	    	});
+	    }
+    };
   });
