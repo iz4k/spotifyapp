42b89dfb3711f0cbdef7662e670aba739f20988e
Meuronen Ilari <meuroila@AL000908.local>
2015-09-22 18:06:32 +0300

    added search view

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyappApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the spotifyappApp
+ */
+angular.module('spotifyappApp')
+  .controller('SearchCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
