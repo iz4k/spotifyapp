05c93c300c076604e95a246ad81d4ab7ac6d689e
Meuronen Ilari <meuroila@AL000908.local>
2015-09-22 18:00:06 +0300

    angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spotifyappApp
+ * @description
+ * # spotifyappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spotifyappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spotifyappApp
+ */
+angular.module('spotifyappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
