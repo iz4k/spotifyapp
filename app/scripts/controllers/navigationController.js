8f9a7deb1f29f6659ddb60351012b131a304772f
Meuronen Ilari <ilari.meuronen@tieto.fi>
2015-12-03 18:32:27 +0200

    nav bar down with pan events

--- app/scripts/controllers/navigationController.js
+++ app/scripts/controllers/navigationController.js
@@ -3,65 +3,57 @@
   angular
     .module('spotifyApp')
     .controller('navController', function ($scope, $log) {
+
       $scope.navOpen = false;
       $scope.openNav = function () {
-   console.log('asfg');
-   $scope.navOpen = !$scope.navOpen;
- }
+        console.log('asfg');
+        $scope.navOpen = !$scope.navOpen;
+      }
 
-//      var e = document.getElementById('side-navigation');
-//      var ham = new Hammer(e);
-//      //            console.log('ham is ', ham);
-//      ham.get('swipe').set({
-//        direction: Hammer.DIRECTION_ALL
-//      });
-//      ham.on('swipe', function (event) {
-//        $log.debug('swiping', event);
-//      });
-//
-//      ham.on('pandown', function (event) {
-//        $(event.target).css({
-//          'transform': 'translateY(0px)'
-//        });
-//      });
-//
-//      ham.on('panup', function (event) {
-//        $(event.target).css({
-//          'transform': 'translateY(-70%)'
-//        });
-//      });
-      //
-      //      ham.on('panend', function (event) {
-      //        console.log('dragend', event.target);
-      //        $(event.target).parent().css({
-      //          'color': 'red'
-      //        });
-      //        $(event.target).css({
-      //          'transform': 'translate(0,0)'
-      //        });
-      //        debugger;
-      //        var dropEl = document.elementFromPoint(event.gesture.center.pageX, event.gesture.center.pageY);
-      //        console.log('dropped on', dropEl);
-      //        if ($(dropEl).hasClass('drop-target')) {
-      //          console.log('dropped on drop target');
-      //        }
-      //      });
-      //
-      //      ham.on('tap', function (event) {
-      //        $log.debug('wat', event);
-      //      });
-      //      ham.on('swipedown', function (event) {
-      //        $scope.navOpen = true;
-      //      })
+      $scope.touchMove = function (event) {
+        var element = angular.element(document.querySelector('#side-navigation'));
+        element.css({
+          'transition': 'none'
+        });
+        var y = event.center.y;
+        if (y > 200) y = 200;
+        element.css({
+          'top': y + 'px'
+        });
+      };
 
-      //      $(document.body).on('mousedown', '[draggable]', function (event) {
+      $scope.touchEnd = function (event) {
+        var element = angular.element(document.querySelector('#side-navigation'));
+        var y = event.center.y;
+        if (y > 100) {
+          element.css({
+            'top': '200px',
+            'transition': 'ease-in 0.25s all'
+          });
+        } else if (y < 100) {
+          element.css({
+            'top': '0px',
+            'transition': 'ease-in 0.25s all'
+          });
+        }
+      };
+      //      DO WE NEED SEPARATE SWIPE SHIT
+      //      $scope.swipeDown = function (event) {
+      //        var element = angular.element(document.querySelector('#side-navigation'));
+      //        console.log('IT WAS SWIPE DOWN');
+      //        element.css({
+      //          'top': '200px',
+      //          'transition': 'ease-in 0.25s all'
+      //        });
+      //      };
       //
-      //        console.log('mousedown', event);
-      //      })
-      //      $(document.body).on('mouseup', '[draggable]', function (event) {
-      //
-      //        console.log('mouseup', event);
-      //        event.preventDefault()
-      //      })
+      //      $scope.swipeUp = function (event) {
+      //        var element = angular.element(document.querySelector('#side-navigation'));
+      //        console.log('IT WAS SWIPE UP');
+      //        element.css({
+      //          'top': '0px',
+      //          'transition': 'ease-in 0.25s all'
+      //        });
+      //      };
     })
 })();
