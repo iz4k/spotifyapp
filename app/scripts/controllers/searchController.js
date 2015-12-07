a0aee753cec3951e30503b6d3b74d6bfbc7ec9e3
Antti Partanen <antti.partanen@aalto.fi>
2015-12-07 03:24:06 +0200

    Added the top songs for the artist search when the artist is tapped. Layout needs fixing tho

--- app/scripts/controllers/searchController.js
+++ app/scripts/controllers/searchController.js
@@ -1,39 +1,64 @@
 (function () {
 
-  angular
+    angular
     .module('spotifyApp')
-    .controller('searchController', function (spotifyService, nowPlayingService, $scope, $log, $timeout) {
-      $scope.searchType = 'Album';
-      $scope.selectedAlbum = '';
-      $scope.results = [];
-      $scope.search = function () {
-        spotifyService.getResults({
-          q: $scope.searchTerm,
-          type: $scope.searchType
-        }).$promise.then(function (data) {
-          if ($scope.searchType == 'Album') {
-            $scope.results = data.albums.items;
-            _.each($scope.results, function (album) {
-              spotifyService.getAlbumDetails({
-                id: album.id
-              }).$promise.then(function (info) {
-                album.info = info;
-              });
+    .controller('searchController', function (spotifyService, spotifyFactory, nowPlayingService, Auth, $scope, $log, $timeout) {
+        $scope.searchType = 'Album';
+        $scope.selectedAlbum = '';
+        $scope.tapped_item_id = '';
+        $scope.tracks = [];
+        $scope.results = [];
+        $scope.search = function () {
+            spotifyService.getResults({
+                q: $scope.searchTerm,
+                type: $scope.searchType
+            }).$promise.then(function (data) {
+                if ($scope.searchType == 'Album') {
+                    $scope.results = data.albums.items;
+                    _.each($scope.results, function (album) {
+                        spotifyService.getAlbumDetails({
+                            id: album.id
+                        }).$promise.then(function (info) {
+                            album.info = info;
+                        });
+                    });
+                } else {
+                    $scope.results = data.artists.items;
+                    console.log($scope.results);
+                }
             });
-          } else {
-            $scope.results = data.artists.items;
-          }
-        });
-      };
+        };
 
-      $scope.selectItem = function(item) {
-        if (item.type == 'album') {
-          // Selected item is an album
-          var tracks = item.info.tracks.items;
-          nowPlayingService.setTracks(tracks, false, 0, item.images[0].url);
-        } else if (item.type == 'artist') {
-          // TODO: Handle artist search
+        $scope.selectItem = function(item) {
+            if (item.type == 'album') {
+                // Selected item is an album
+                var tracks = item.info.tracks.items;
+                nowPlayingService.setTracks(tracks, false, 0, item.images[0].url);
+            } else if (item.type == 'artist') {
+                // TODO: Handle artist search
+                // GET top popular songs
+                
+                console.log('artist tapped')
+                
+                if ($scope.tapped_item_id == item.id) {
+                    $scope.tapped_item_id = '';
+
+                } else {
+
+                    $scope.tapped_item_id = item.id
+                        spotifyFactory.getArtistTopTracks(item.id,Auth.getUserCountry()).then(function(data) {
+                            console.log('got top tracks ', data);
+                            $scope.tracks=data.tracks;
+                        });     
+                }
+
+
+            }
+        };
+
+        $scope.pinchOut = function(item) {
+            console.log('Pinched out' + item.name);
         }
-      };
+
     })
 })();
--- app/scripts/services/SpotifyFactory.js
+++ app/scripts/services/SpotifyFactory.js
@@ -1,135 +1,148 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  /* This is doing the same stuff as the spotifyService, but for 
-     some reason the login works only properly with this. 
+    /* This is doing the same stuff as the spotifyService, but for 
+       some reason the login works only properly with this. 
 
 */
 
-  var module = angular.module('spotifyApp');
+    var module = angular.module('spotifyApp');
 
-  module.factory('spotifyFactory', function (Auth, $q, $http) {
-    var baseUrl = 'https://api.spotify.com/v1';
+    module.factory('spotifyFactory', function (Auth, $q, $http) {
+        var baseUrl = 'https://api.spotify.com/v1';
 
-    return {
-      userInformation: function() {
-        var ret = $q.defer();
-        $http.get(baseUrl + '/me', {
-          headers: {
-            'Authorization': 'Bearer ' + Auth.getAccessToken()
-          }
-        }).success(function(r) {
-          console.log('got userinfo', r);
-          ret.resolve(r);
-        }).error(function(err) {
-          console.log('failed to get userinfo', err);
-          ret.reject(err);
-        });
-        return ret.promise;
-      },
+        return {
+            userInformation: function() {
+                var ret = $q.defer();
+                $http.get(baseUrl + '/me', {
+                    headers: {
+                        'Authorization': 'Bearer ' + Auth.getAccessToken()
+                    }
+                }).success(function(r) {
+                    console.log('got userinfo', r);
+                    ret.resolve(r);
+                }).error(function(err) {
+                    console.log('failed to get userinfo', err);
+                    ret.reject(err);
+                });
+                return ret.promise;
+            },
 
-    getPlaylists: function(username) {
-      var limit = 50;
-      var ret = $q.defer();
-      var playlists = [];
+        getPlaylists: function(username) {
+            var limit = 50;
+            var ret = $q.defer();
+            var playlists = [];
 
-      $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists', {
-        params: {
-          limit: limit
+            $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists', {
+                params: {
+                    limit: limit
+                },
+                headers: {
+                    'Authorization': 'Bearer ' + Auth.getAccessToken()
+                }
+            }).success(function(r) {
+                playlists = playlists.concat(r.items);
+
+                var promises = [],
+            total = r.total,
+            offset = r.offset;
+
+            while (total > limit + offset) {
+                promises.push(
+                    $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists', {
+                        params: {
+                            limit: limit,
+                        offset: offset + limit
+                        },
+                        headers: {
+                            'Authorization': 'Bearer ' + Auth.getAccessToken()
+                        }
+                    })
+                    );
+                offset += limit;
+            };
+
+            $q.all(promises).then(function(results) {
+                results.forEach(function(result) {
+                    playlists = playlists.concat(result.data.items);
+                })
+                //console.log('got playlists', playlists);
+                ret.resolve(playlists);
+            });
+
+            }).error(function(data, status, headers, config) {
+                ret.reject(status);
+            });
+            return ret.promise;
         },
-        headers: {
-          'Authorization': 'Bearer ' + Auth.getAccessToken()
-        }
-      }).success(function(r) {
-        playlists = playlists.concat(r.items);
 
-        var promises = [],
-      total = r.total,
-      offset = r.offset;
+        getPlaylist: function(username, playlist) {
+            var ret = $q.defer();
+            $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists/' + encodeURIComponent(playlist), {
+                headers: {
+                    'Authorization': 'Bearer ' + Auth.getAccessToken()
+                }
+            }).success(function(r) {
+                ret.resolve(r);
+            });
+            return ret.promise;
+        },
 
-      while (total > limit + offset) {
-        promises.push(
-          $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists', {
-            params: {
-              limit: limit,
-            offset: offset + limit
-            },
-            headers: {
-              'Authorization': 'Bearer ' + Auth.getAccessToken()
-            }
-          })
-          );
-        offset += limit;
-      };
+        removeTrackFromPlaylist: function(username, playlist, track, position) {
+            var ret = $q.defer();
+            $http.delete(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists/' + encodeURIComponent(playlist) + '/tracks',
+                    {
+                        data: {
+                            tracks: [{
+                                uri: track.uri,
+                position: position
+                            }]
+                        },
+                headers: {
+                    'Authorization': 'Bearer ' + Auth.getAccessToken()
+                }
+                    }).success(function(r) {
+                        console.log('remove track from playlist', r);
+                        ret.resolve(r);
+                    });
+            return ret.promise;
+        },
 
-      $q.all(promises).then(function(results) {
-        results.forEach(function(result) {
-          playlists = playlists.concat(result.data.items);
-        })
-        //console.log('got playlists', playlists);
-        ret.resolve(playlists);
-      });
+        createNewPlaylist: function(playlistname) {
+            var ret = $q.defer();
+            $http.post(baseUrl + '/users/' + encodeURIComponent(Auth.getUsername()) + '/playlists',
+                    {
+                        data: {
+                            name: playlistname,
+                            public: true
+                        },
+                headers: {
+                    'Authorization': 'Bearer ' + Auth.getAccessToken()
+                }
+                    }).success(function(r) {
+                        console.log('Added a new playlist', r);
+                        ret.resolve(r);
+                    });
+            return ret.promise;
+        },
 
-      }).error(function(data, status, headers, config) {
-        ret.reject(status);
-      });
-      return ret.promise;
-    },
-
-    getPlaylist: function(username, playlist) {
-      var ret = $q.defer();
-      $http.get(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists/' + encodeURIComponent(playlist), {
-        headers: {
-          'Authorization': 'Bearer ' + Auth.getAccessToken()
-        }
-      }).success(function(r) {
-        ret.resolve(r);
-      });
-      return ret.promise;
-    },
-
-    removeTrackFromPlaylist: function(username, playlist, track, position) {
-      var ret = $q.defer();
-      $http.delete(baseUrl + '/users/' + encodeURIComponent(username) + '/playlists/' + encodeURIComponent(playlist) + '/tracks',
-          {
-            data: {
-              tracks: [{
-                uri: track.uri,
-        position: position
-              }]
-            },
-        headers: {
-          'Authorization': 'Bearer ' + Auth.getAccessToken()
-        }
-          }).success(function(r) {
-            console.log('remove track from playlist', r);
-            ret.resolve(r);
-          });
-      return ret.promise;
-    },
-
-    createNewPlaylist: function(playlistname) {
-      var ret = $q.defer();
-      $http.post(baseUrl + '/users/' + encodeURIComponent(Auth.getUsername()) + '/playlists',
-          {
-            data: {
-              name: playlistname,
-              public: true
-            },
-        headers: {
-          'Authorization': 'Bearer ' + Auth.getAccessToken()
-        }
-          }).success(function(r) {
-            console.log('Added a new playlist', r);
-            ret.resolve(r);
-          });
-      return ret.promise;
-    },
+        getArtistTopTracks: function(artistid, country) {
+            var ret = $q.defer();
+            $http.get(baseUrl + '/artists/' + encodeURIComponent(artistid) + '/top-tracks?country=' + encodeURIComponent(country), {
+                headers: {
+                    'Authorization': 'Bearer ' + Auth.getAccessToken()
+                }
+            }).success(function(r) {
+                ret.resolve(r);
+            });
+            return ret.promise;
+        },
 
 
 
-    }   
 
-  });
+        }   
+
+    });
 
 })();
